c005e5892248def75bfc1e5ca9a4768901d3ab15
Strajde <patrykgorny1@gmail.com>
2022-06-07 20:42:57 +0200

    all https connected with database

--- AnimalsAPI/Controllers/AnimalsController.cs
+++ AnimalsAPI/Controllers/AnimalsController.cs
@@ -7,10 +7,7 @@ namespace AnimalsAPI.Controllers
     [ApiController]
     public class AnimalsController : ControllerBase
     {
-        private static List<Animals> animals = new List<Animals>
-            {
-                
-            };
+        
         private readonly DataContext context;
 
         public AnimalsController(DataContext context)
@@ -41,9 +38,10 @@ namespace AnimalsAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<List<Animals>>> AddAnimal(Animals animal)
         {
-            animals.Add(animal);
+            context.Animals.Add(animal);
+            await context.SaveChangesAsync();
 
-            return Ok(animals);
+            return Ok(await context.Animals.ToListAsync());
         }
 
         //Put
@@ -51,16 +49,18 @@ namespace AnimalsAPI.Controllers
         [HttpPut]
         public async Task<ActionResult<List<Animals>>> UpdateAnimal(Animals request)
         {
-            var animal = animals.Find(x => x.Id == request.Id);
-            if (animal == null)
+            var dbAnimal = await context.Animals.FindAsync(request.Id);
+            if (dbAnimal == null)
                 return BadRequest("Animal not found.");
 
-            animal.Name = request.Name;
-            animal.FirstName = request.FirstName;
-            animal.LastName = request.LastName;
-            animal.Place = request.Place;
+            dbAnimal.Name = request.Name;
+            dbAnimal.FirstName = request.FirstName;
+            dbAnimal.LastName = request.LastName;
+            dbAnimal.Place = request.Place;
 
-            return Ok(animals);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.Animals.ToListAsync());
         }
 
         //Delete
@@ -68,13 +68,14 @@ namespace AnimalsAPI.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<Animals>>> Delete(int id)
         {
-            var animal = animals.Find(x => x.Id == id);
-            if (animal == null)
+            var dbAnimal = await context.Animals.FindAsync(id);
+            if (dbAnimal == null)
                 return BadRequest("Animal not found.");
 
-            animals.Remove(animal);
+            context.Animals.Remove(dbAnimal);
+            await context.SaveChangesAsync();
 
-            return Ok(animals);
+            return Ok(await context.Animals.ToListAsync());
         }
 
     }
